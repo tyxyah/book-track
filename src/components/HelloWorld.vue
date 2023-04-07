484b6ba7070c2a3a9117cc85fc7b1f1e2f5296ce
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-04-07 21:53:30 +0800

    tesh

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
         src="@/assets/logo.svg"
       />
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome Aisyah</div>
+      <div class="text-body-2 font-weight-light mb-n1">Welkomin</div>
 
       <h1 class="text-h2 font-weight-bold">BookTrack</h1>
 
