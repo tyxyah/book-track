7214898aa247b1ca6f1398469448e89bd498f56a
zaimizain <zaimizain07@gmail.com>
2023-04-07 15:44:16 +0800

    Update AppBar2
    hdbvdfhjbvh

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,21 +1,44 @@
 <template>
-  <div>
+  <v-card flat>
     <v-toolbar
+      color="primary"
       dark
-      prominent
-      image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      extended
+      flat
     >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Vuetify</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-export</v-icon>
-      </v-btn>
     </v-toolbar>
-  </div>
+
+    <v-card
+      class="mx-auto"
+      max-width="700"
+      style="margin-top: -64px;"
+    >
+      <v-toolbar flat>
+        <v-toolbar-title class="text-grey">
+          Title
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-apps</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <v-card-text style="height: 200px;"></v-card-text>
+    </v-card>
+  </v-card>
 </template>
 
 <script setup>
