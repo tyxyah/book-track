0e65f450e721d5bb48d12a8fb441ede052bf81a7
zaimizain <zaimizain07@gmail.com>
2023-05-03 00:02:18 +0800

    BarAgain

--- src/components/AkuTry.vue
+++ src/components/AkuTry.vue
@@ -1,26 +1,43 @@
 <template>
- <div>
-
-    <v-toolbar color="primary">
-
-
+ 
+ 
+     <v-toolbar color="red" density="compact">
+ 
+       
+ 
+       
       <v-toolbar-title>BookTrack</v-toolbar-title>
-     
-      <v-text-field
-        :loading="loading"
-        density="compact"
-        variant="solo"
-        label="Search Book"
-        append-inner-icon="mdi-magnify"
-        single-line
-        hide-details
-        @click:append-inner="onClick"
-      ></v-text-field>
-     
-      <v-toolbar-title>BookTrack</v-toolbar-title>
-   
-</v-toolbar>
+      
 
- </div>
+
+       <v-spacer></v-spacer>
+       <v-text-field
+         :loading="loading"
+         density="compact"
+         variant="solo"
+         label="Search Book"
+         append-inner-icon="mdi-magnify"
+         single-line
+         hide-details
+         @click:append-inner="onClick"
+       ></v-text-field>
+       <v-divider
+      class="ms-3"
+      inset
+      vertical
+    ></v-divider>
+       <v-checkbox label="Availability"></v-checkbox>
+       <v-divider
+      class="ms-3"
+      inset
+      vertical
+    ></v-divider>
+    <v-btn>Search</v-btn>
+    
+ </v-toolbar>
+
   
-</template>
+  
+  
+   
+ </template>
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,32 +1,29 @@
 <template>
-  
+  <div>
  
-    <v-toolbar density="compact">
-
-      <v-toolbar-title>BookTrack</v-toolbar-title>
-
-      <v-text-field
-        :loading="loading"
-        density="compact"
-        variant="solo"
-        label="Search Book"
-        append-inner-icon="mdi-magnify"
-        single-line
-        hide-details
-        @click:append-inner="onClick"
-      ></v-text-field>
-
-      <v-row class="ma-2" justify="right">
-     
-      </v-row>
-      <v-checkbox label="Availability"></v-checkbox>
-      <v-btn>
-        Search
-      </v-btn>
-    </v-toolbar>
-
-</template>
-
-<script setup>
-  //
-</script>
+     <v-toolbar color="primary" density="compact">
+ 
+       <v-toolbar-title>BookTrack</v-toolbar-title>
+ 
+       
+       <v-text-field
+         :loading="loading"
+         density="compact"
+         variant="solo"
+         label="Search Book"
+         append-inner-icon="mdi-magnify"
+         single-line
+         hide-details
+         @click:append-inner="onClick"
+       ></v-text-field>
+       <v-checkbox label="Availability"></v-checkbox>
+       <v-btn >
+         Search
+       </v-btn>
+ </v-toolbar>
+ 
+  </div>
+ 
+  
+   
+ </template>
