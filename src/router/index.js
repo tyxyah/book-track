1fb62c16ff5c16275d68ef2532a4705fdd41986b
Siti Aisyah Binti Razali <78149732+tyxyah@users.noreply.github.com>
2023-05-26 16:06:04 +0800

    Update index.js
    Has define all page path

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,41 @@ const routes = [
       {
         path: '',
         name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
     ],
+  },
+  {
+    path: '/book-detail',
+    component: () => import('@/components/BookDetail.vue'),
+  },
+  {
+    path: '/log-in',
+    component: () => import('@/components/Login.vue'),
+  },
+  {
+    path: '/book-edit',
+    component: () => import('@/components/EditBook.vue'),
+  },
+  {
+    path: '/book-detail',
+    component: () => import('@/components/BookDetail.vue'),
+  },
+  {
+    path: '/search-book',
+    component: () => import('@/components/SearchBook.vue'),
+  },
+  {
+    path: '/home-page',
+    component: () => import('@/components/MainPage.vue'),
+  },
+  {
+    path: '/sign-up',
+    component: () => import('@/components/Signup.vue'),
+  },
+  {
+    path: '/add-book',
+    component: () => import('@/components/AddBook.vue'),
   },
 ]
 
