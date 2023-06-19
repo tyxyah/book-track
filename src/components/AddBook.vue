9c01c0a27c92d5a68c7908bae11410c0a22d67f7
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-06-19 21:52:57 +0800

    Update AddBook.vue

--- src/components/AddBook.vue
+++ src/components/AddBook.vue
@@ -229,7 +229,7 @@
                 :src="
                   image_name
                     ? image_name
-                    : '/src/assets/no.png'
+                    : '/public/image-not-available.jpeg'
                 "
               >
               </v-img>
